8818440bc19f64b70230a21b5ab9d8d9bbb71fb0
Kelvin Liang <kelvin9877@gmail.com>
2020-07-23 11:01:14 +0800

    all posts migrate to mdx and update project page

---
+++ src/components/ProjectList/ProjectList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ProjectList.module.scss';
+
+interface props {
+  projectList: Array<string>;
+}
+
+const ProjectList = ({ projectList }: props) => {};
+
+export default ProjectList;
